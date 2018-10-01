6aa16c5237fa31a27d7a9037832cc881ed981519
llNovall <nels.puthoor@hotmail.com>
2018-10-01 17:58:17 +0100

    Model Class Set

---
+++ GameShopNew/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GameShopNew.Models
+{
+    public enum ItemCategory { Health, Stamina, Magic }
+    public class Item
+    {
+        ItemCategory Category;
+
+        [DataType("decimal(18,2)")]
+        double Cost;
+        string IconURL;
+
+        [Required]
+        int ID;
+
+        [MaxLength(50)]
+        string Name;
+
+        
+    }
+}
